c6e6d0e6e63970cf3596958356112771e16f8446
Timothy Putzke <tsputzke@gmail.com>
2020-01-07 20:36:41 -0500

    Implement menu class toggle

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      class: 'closed'
+    }
+  }  
+
+  toggleMenu = () => {
+    if(this.state.class === 'closed') {
+      this.setState({class: 'open'});
+    } else {
+      this.setState({class: 'closed'});
+    }
+  }
+
   render() {
     return (
       <div className='root_wrapper'>
         <header>
-          <div className='menu'>
-            <span className='menu_burger'></span>
+          <div className='menu' onClick={this.toggleMenu}>
+            <span className={`menu_burger ${this.state.class}`}></span>
           </div>
 
-          <nav className='nav'>
-            <ul className="menu-nav">
-              <li className="menu-nav_item active">
-                <a href="index.html" className="menu-nav_link">Home</a>
+          <nav className={`nav ${this.state.class}`}>
+            <ul className={`menu-nav ${this.state.class}`}>
+              <li className={`menu-nav_item active ${this.state.class}`}>
+                <a href='index.html' className='menu-nav_link'>Home</a>
               </li>
-              <li className="menu-nav_item">
-                <a href="about.html" className="menu-nav_link">Inventory</a>
+              <li className={`menu-nav_item ${this.state.class}`}>
+                <a href='about.html' className='menu-nav_link'>Inventory</a>
               </li>
-              <li className="menu-nav_item">
-                <a href="projects.html" className="menu-nav_link">Add Set</a>
+              <li className={`menu-nav_item ${this.state.class}`}>
+                <a href='projects.html' className='menu-nav_link'>Add Set</a>
+              </li>
+              <li className={`menu-nav_item ${this.state.class}`}>
+                <a href='projects.html' className='menu-nav_link'>Logout</a>
               </li>
             </ul>
           </nav>
