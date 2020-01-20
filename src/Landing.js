f751b6ccb87bc4ef4e2e6c54cf86df5709d27ac9
Timothy Putzke <tsputzke@gmail.com>
2020-01-20 12:04:22 -0500

    Style landing, implement font awesome icons

--- src/Landing.js
+++ src/Landing.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import FA from 'react-fontawesome';
 import AuthApiService from '../src/services/auth-api-service'
 import TokenService from '../src/services/token-service'
 
@@ -64,40 +65,51 @@ class Landing extends Component {
             <h1>Bricks and Builds</h1>
             <h5>LEGO Curation Made Easy</h5>
           </section>
-          <section className='landing_hero'>
-            <h4><Link to='/registration'><span className='link_span'> Create an account</span></Link> to start organizing your builds like a zen master</h4>
-          </section>
           <section id='login'>
             <form 
               id='login-form'
               onSubmit={this.handleLogin}  
             >
               <fieldset>
-                <legend>User Login:</legend>
+                <legend>Member Login</legend>
                 <div>
                   <label htmlFor="user_name">Username: </label>
+                  <br/>
                   <input type="text" name='user_name' id='login-user_name' />
                 </div>
                 <div>
                   <label htmlFor="password">Password: </label>
+                  <br/>
                   <input type="password" name='password' id='login-password' />
                 </div>
                 <button type='submit'>Login</button>
               </fieldset>
+              <h6>Not registered? <br/> <Link to='/registration'><span className='link_span'> Create an account!</span></Link></h6>
               <div role='alert'>
                 {error && <p className='error'>{error}</p>}
               </div>
             </form>
           </section>
           <section id='about_section'>
-            <h2>About</h2>
-            <p>Want to create a digital inventory of your LEGO collection? <span className='emphasize'>Bricks and Builds.</span></p>
-            <br/>
-            <p>Want to explore some amazing alternate builds possible with the pieces that you already have in your collection? <span className='emphasize'>Bricks and Builds.</span></p>
-            <br/>
-            <p>Want to save your favorites builds so you can go back to them any time you want? That's right. <span className='emphasize'>Bricks and Builds.</span></p>
-            <br/>
-            <p>Login to a <span className='link_span' onClick={() => this.handleDemoLogin()}>demo account</span> to see for yourself.</p>
+            <table>
+              <tbody>
+                <tr>
+                  <div>
+                    <td><FA className='about_icon' name='plus-circle'/> </td>
+                    <td>Create a digital inventory of your LEGO collection</td>
+                  </div>
+                  <div>
+                    <td><FA className='about_icon' name='search'/></td>
+                    <td>Explore amazing alternate builds possible with the pieces you already have</td>
+                  </div>
+                  <div>
+                    <td><FA className='about_icon' name='save'/></td>
+                    <td>Save your favorite builds to revisit any time</td>
+                  </div>
+                </tr>
+              </tbody>
+            </table>
+            <span className='link_span' onClick={() => this.handleDemoLogin()}>Login to a demo account</span>
           </section>
         </main>
       </div>
